2e0ac0cfa0e7309ab287f84966f85d673b07a1bc
Adraelsen <adrianrayishmael@gmail.com>
2020-06-17 02:28:13 -0500

    Added basic Driver

---
+++ src/main/java/com/ishmael/spotit/Driver.java
@@ -0,0 +1,11 @@
+package com.ishmael.spotit;
+
+public class Driver {
+    public static void main(String[] args) {
+        new Driver();
+    }
+
+    public Driver() {
+        System.out.println("Hello World");
+    }
+}
