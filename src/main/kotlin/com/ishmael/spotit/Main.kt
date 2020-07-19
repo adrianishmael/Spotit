a05af7e76e3854c4758f3dac2c4d9aea17818948
Adraelsen <adrianrayishmael@gmail.com>
2020-07-18 23:41:55 -0500

    Added getting of playlist by Spotify username

--- src/main/kotlin/com/ishmael/spotit/Main.kt
+++ src/main/kotlin/com/ishmael/spotit/Main.kt
@@ -4,13 +4,18 @@ import com.adamratzman.spotify.spotifyAppApi
 import kotlin.system.exitProcess
 
 fun main() {
-    val api = spotifyAppApi(
-            System.getenv("CLIENT_ID"),
-            System.getenv("CLIENT_SECRET"))
-            .build()
+    val api = spotifyAppApi(System.getenv("CLIENT_ID"), System.getenv("CLIENT_SECRET")).build()
 
-    val fobID = api.search.searchArtist("Blink-182").complete()[0].id
-    println(api.artists.getArtistTopTracks(fobID).complete().joinToString { it.name })
-    println("Hello World")
+    println("Enter Spotify username")
+    val userName = readLine() as String
+
+    try {
+        val playlistSearch = api.playlists.getUserPlaylists(userName).complete().map { it!!.name }
+        println(playlistSearch)
+    }
+    catch (e: com.adamratzman.spotify.models.SpotifyUriException) {
+        println("The following error has occurred: " + e.message)
+    }
+
     exitProcess(0)
 }
