ace8113d5decd2d0c7776ecec934dff1b37235e5
Adraelsen <adrianrayishmael@gmail.com>
2020-07-19 21:05:59 -0500

    Added data file to save username

--- src/main/kotlin/com/ishmael/spotit/Main.kt
+++ src/main/kotlin/com/ishmael/spotit/Main.kt
@@ -1,16 +1,18 @@
 package com.ishmael.spotit
 
 import com.adamratzman.spotify.spotifyAppApi
+import java.io.File
+import java.io.FileNotFoundException
 import kotlin.system.exitProcess
 
 fun main() {
     val api = spotifyAppApi(System.getenv("CLIENT_ID"), System.getenv("CLIENT_SECRET")).build()
 
-    println("Enter Spotify username")
-    val userName = readLine() as String
+    val username = getUsername()
 
     try {
-        val playlistSearch = api.playlists.getUserPlaylists(userName).complete().map { it!!.name }
+        val playlistSearch = api.playlists.getUserPlaylists(username).complete().map { it!!.name }
+        println("User's playlists: ")
         println(playlistSearch)
     }
     catch (e: com.adamratzman.spotify.models.SpotifyUriException) {
@@ -18,4 +20,21 @@ fun main() {
     }
 
     exitProcess(0)
+}
+
+fun getUsername(): String {
+    return if(File("src/main/resources/data.txt").exists()) {
+        File("src/main/resources/data.txt").readText()
+    }
+    else {
+        println("Enter Spotify username")
+        val username = readLine()!!.trim()
+
+        println("Save username for later use? (y/n)")
+        when(readLine()) {
+            "y" -> File("src/main/resources/data.txt").writeText(username)
+        }
+
+        username
+    }
 }
