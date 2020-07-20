8f7565629ce24448b5abbcd34d403ae9f3d3ba7d
Adraelsen <adrianrayishmael@gmail.com>
2020-07-20 00:50:20 -0500

    Updated getting user's playlists to its own function

--- src/main/kotlin/com/ishmael/spotit/Main.kt
+++ src/main/kotlin/com/ishmael/spotit/Main.kt
@@ -4,14 +4,39 @@ import com.adamratzman.spotify.spotifyAppApi
 import java.io.File
 import kotlin.system.exitProcess
 
-fun main() {
-    val api = spotifyAppApi(System.getenv("CLIENT_ID"), System.getenv("CLIENT_SECRET")).build()
-    val username = getUsername()
+private val api = spotifyAppApi(System.getenv("CLIENT_ID"), System.getenv("CLIENT_SECRET")).build()
 
+fun main() {
+    val username = getUsername()
+    val playlistSearch = getUserPlaylists(username)
+    println("User's playlists: ")
+    println(playlistSearch)
+
+    exitProcess(0)
+}
+
+fun getUsername(): String {
+    val pathname = "src/main/resources/data.txt"
+
+    return if(File(pathname).exists()) {
+        File(pathname).readText()
+    }
+    else {
+        println("Enter Spotify username")
+        val username = readLine()!!.trim()
+
+        println("Save username for later use? (y/n)")
+        when(readLine()) {
+            "y" -> File(pathname).writeText(username)
+        }
+
+        username
+    }
+}
+
+fun getUserPlaylists(username: String): List<String> {
     try {
-        val playlistSearch = api.playlists.getUserPlaylists(username).complete().map { it!!.name }
-        println("User's playlists: ")
-        println(playlistSearch)
+        return api.playlists.getUserPlaylists(username).complete().map { it!!.name }
     }
     catch(e: Exception) {
         if(File("src/main/resources/data.txt").exists())
@@ -20,24 +45,5 @@ fun main() {
         println("An error has occurred. This is most likely because an incorrect username was provided")
         println("Double check your username is correct on next run")
         exitProcess(1)
-    }
-
-    exitProcess(0)
-}
-
-fun getUsername(): String {
-    return if(File("src/main/resources/data.txt").exists()) {
-        File("src/main/resources/data.txt").readText()
-    }
-    else {
-        println("Enter Spotify username")
-        val username = readLine()!!.trim()
-
-        println("Save username for later use? (y/n)")
-        when(readLine()) {
-            "y" -> File("src/main/resources/data.txt").writeText(username)
-        }
-
-        username
     }
 }
