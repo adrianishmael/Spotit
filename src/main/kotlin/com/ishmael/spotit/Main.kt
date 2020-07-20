72a25a04554a7951c68993f3cbcc8132e2d93260
Adraelsen <adrianrayishmael@gmail.com>
2020-07-19 21:18:45 -0500

    Added error handling for invalid username

--- src/main/kotlin/com/ishmael/spotit/Main.kt
+++ src/main/kotlin/com/ishmael/spotit/Main.kt
@@ -2,12 +2,10 @@ package com.ishmael.spotit
 
 import com.adamratzman.spotify.spotifyAppApi
 import java.io.File
-import java.io.FileNotFoundException
 import kotlin.system.exitProcess
 
 fun main() {
     val api = spotifyAppApi(System.getenv("CLIENT_ID"), System.getenv("CLIENT_SECRET")).build()
-
     val username = getUsername()
 
     try {
@@ -15,8 +13,13 @@ fun main() {
         println("User's playlists: ")
         println(playlistSearch)
     }
-    catch (e: com.adamratzman.spotify.models.SpotifyUriException) {
-        println("The following error has occurred: " + e.message)
+    catch(e: Exception) {
+        if(File("src/main/resources/data.txt").exists())
+            File("src/main/resources/data.txt").delete()
+
+        println("An error has occurred. This is most likely because an incorrect username was provided")
+        println("Double check your username is correct on next run")
+        exitProcess(1)
     }
 
     exitProcess(0)
