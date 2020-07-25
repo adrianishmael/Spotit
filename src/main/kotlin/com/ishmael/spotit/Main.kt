9877c045ca739c7f14f1b8320aba3779bb287651
Adraelsen <adrianrayishmael@gmail.com>
2020-07-25 18:18:47 -0500

    Changed username storage path and fixed missing directory bug:

--- src/main/kotlin/com/ishmael/spotit/Main.kt
+++ src/main/kotlin/com/ishmael/spotit/Main.kt
@@ -22,18 +22,19 @@ fun main() {
 }
 
 fun getUsername(): String {
-    val pathname = "src/main/resources/data.txt"
+    val pathname = "data/username.dat"
 
     return if(File(pathname).exists()) {
         File(pathname).readText()
     }
     else {
+        File("data").mkdir() // make the data directory so we don't crash
+
         println("Enter Spotify username")
         val username = readLine()!!.trim()
 
         println("Save username for later use? (y/n)")
         when(readLine()) {
-            // TODO: if resources package is not present FileNotFoundException is thrown
             "y" -> File(pathname).writeText(username)
         }
 
@@ -46,8 +47,9 @@ fun getUserPlaylists(): PagingObject<SimplePlaylist> {
         return api.playlists.getUserPlaylists(username).complete()
     }
     catch(e: Exception) {
-        if(File("src/main/resources/data.txt").exists())
-            File("src/main/resources/data.txt").delete()
+        if(File("data/username.dat").exists())
+            File("data/username.dat").delete()
+            // TODO: Delete here so we can get a correct username on next run. To change later when commands are added
 
         println("An error has occurred. This is most likely because an incorrect username was provided")
         println("Double check your username is correct on next run")
